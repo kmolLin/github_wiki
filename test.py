762d97970fec58849aa38e271bc11b0c21634316
smpss91341 <smpss91341@gmail.com>
2015-03-22 19:02:16 +0800

    read the files

--- test.py
+++ test.py
@@ -2,22 +2,35 @@ import datetime
 import os 
 os.environ['tz'] = 'Asia/Tapei'
 
+for x in range(1,18):
+  #  a.append("2015wcm_g"+str(x))   array+1
+    #print ("2015wcm_g"+str(x)+".md")   debug use 
+ 
 #open the file 
-op = open("2015wcm_g1.md","r+")
+    op = open("2015wcm.wiki/2015wcm_g"+str(x)+".md","r+",encoding = 'UTF-8')
 
-'''op.md 內容
-40323101
-1.about me
-2.linkedin
-3.wordpress
-'''
-data = []
-for line in op.readlines():
-    data.append(line)
+
+#op.md 內容
+#40323101
+#1.about me
+#2.linkedin
+#3.wordpress
+
+    while True :
+        i = op.readline()
+        if i =='':
+            break
+        print(i ,end='')
+
+    op.close()
+
+#ata = []
+#or line in op.readlines():
+  # data.append(line)
     
 
-op.close()
+#p.close()
 
-clas_number = data[0]
+#las_number = data[0]
 
-print(clas_number)
+#rint(clas_number)
